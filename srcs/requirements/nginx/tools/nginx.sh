d0feb9d2a66a5aae9ca5881bb666d0a941ebe36e
Lea Medrano <leamedrano@Leas-MacBook-Pro.local>
2024-12-12 11:15:45 +0100

    omg it works

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -6,4 +6,5 @@ openssl req -x509 -nodes \
 	-out /etc/nginx/ssl/nginx.crt \
 	-keyout /etc/nginx/ssl/nginx.key -subj \
 	"/C=CH/ST=Vaud/L=Lausanne/O=42/OU=XXX/CN=$DOMAIN_NAME"
+
 /usr/sbin/nginx -g "daemon off;"
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,25 +1,16 @@
 #!/bin/sh
 
+cd /var/www/html
+
 while ! mysql -h mariadb -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE; do
   sleep 3
 done
 
 echo "Connection successful with db"
 
+wp core download --allow-root
+wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$MYSQL_HOST" --allow-root
+wp core install --allow-root --url="$WP_DOMAINNAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_email="$WP_ADMIN_EMAIL" --admin_password="$WP_ADMIN_PASSWORD"
+wp user create "$WP_USER" "$WP_USER_EMAIL" --role=author --user_pass="$WP_USER_PASSWORD" --allow-root
 
-mkdir /var/www/html/wordpress
-cd /var/www/html/wordpress
-
-wp core download
-
-wp core config --dbname="${MYSQL_DATABASE}" --dbuser="${MYSQL_USER}" --dbpass="${MYSQL_PASSWORD}" \
-		--dbhost=mariadb --locale=en_US --debug=true;
-
-wp core install --url="${WP_DOMAINNAME}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_USER}" \
-	--admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}" --locale=en_US --skip-email
-
-sleep 3
-
-wp user create "${WP_USER}" "${WP_USER_EMAIL}" --user_pass="${WP_USER_PASSWORD}" --role=author
-
-exec /usr/sbin/php-fpm82 -F
+exec /usr/sbin/php-fpm8.2 -F -R
